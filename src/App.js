38272affa50b3d630ad1f2909829ef2efac72a10
Nitesh Nagpal <niteshnagpal@outlook.com>
2021-06-15 18:35:52 +0530

    homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
-import Home from "./screens/HomeScreen/index.jsx";
-import About from "./screens/AboutScreen/index.jsx";
-import Contact from "./screens/ContactScreen/index.jsx";
-import Login from "./screens/LoginScreen/index.jsx";
-import Shop from "./screens/ShopScreen/index.jsx";
+import Home from "./screens/HomeScreen/index";
+import About from "./screens/AboutScreen/index";
+import Contact from "./screens/ContactScreen/index";
+import Login from "./screens/LoginScreen/index";
+import Shop from "./screens/ShopScreen/index";
 
 function App() {
   return (
-    <>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/signin" component={Login} />
-        <Route path="/shop" component={Shop} />
-        <Redirect to="/"></Redirect>
-      </Switch>
-    </>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
+      <Route path="/signin" component={Login} />
+      <Route path="/shop" component={Shop} />
+      <Redirect to="/"></Redirect>
+    </Switch>
   );
 }
 
--- src/screens/HomeScreen/index.jsx
+++ src/screens/HomeScreen/index.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import bgVideo from "../../assets/videos/bgtunnel.mp4"
+import "./home.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="container-fluid home">
+      {/* Background */}
+      <div className="overlay"></div>
+      <div className="bgvideo">
+        <video autoPlay loop muted>
+          <source src={bgVideo} />
+        </video>
+      </div>
+      {/* Main */}
+      <div className="homeinner">
+        <div className="homecnt">
+          <div className="title">
+            <h1>Soft-o-Store</h1>
+          </div>
+          <div className="tagline">
+            <p>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
