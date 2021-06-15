38b9af5e374f7770454a456671a152af3b217345
Nitesh Nagpal <niteshnagpal@outlook.com>
2021-06-15 16:54:47 +0530

    folders done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Home from "./screens/HomeScreen/index.jsx";
+import About from "./screens/AboutScreen/index.jsx";
+import Contact from "./screens/ContactScreen/index.jsx";
+import Login from "./screens/LoginScreen/index.jsx";
+import Shop from "./screens/ShopScreen/index.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/signin" component={Login} />
+        <Route path="/shop" component={Shop} />
+        <Redirect to="/"></Redirect>
+      </Switch>
+    </>
   );
 }
 
